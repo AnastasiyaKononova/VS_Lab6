375cf8470cbe9c0ce8d98169c134a9609a888ee4
Anastasiya <anastkononova@gmail.com>
2017-01-08 02:02:41 +0300

    Change the number of lines of output

--- test.sh
+++ test.sh
@@ -2,10 +2,8 @@ sudo insmod hello_timer.ko
 
 sudo chmod a+w+r /sys/timer/interval
 
-echo "#1"
-echo "10" > /sys/timer/interval
+echo "7" > /sys/timer/interval
 
-sleep 6
+sleep 10
 echo 0 > /sys/timer/interval
 
-sudo rmmod hello_timer
